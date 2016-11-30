6f498308f0f4d53696b30a481868ec262c45dd40
Michael Basil <michael@intrications.com>
2016-11-30 20:28:35 +0000

    Restore files deleted in commit 83a0987

---
+++ httpagent/src/main/java/com/studioidan/httpagent/HTTP.java
@@ -0,0 +1,11 @@
+package com.studioidan.httpagent;
+
+/**
+ * Created by PopApp_laptop on 29/05/16.
+ */
+public class HTTP {
+    public static String GET = "GET";
+    public static String POST = "POST";
+    public static String PUT = "PUT";
+    public static String DELETE = "DELETE";
+}
